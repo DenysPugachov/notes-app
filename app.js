82d0e72f3b80d99a0e9e9cc1b09e109b7c284991
Denys Pugachov <pugachovdenys@gmail.com>
2022-12-21 19:21:30 +0100

    add note.js file

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const command = process.argv[2]
+const chalk = require("chalk")
+const yargs = require("yargs")
+const { getNote } = require("./notes.js")
 
+yargs.command({
+    command: "add",
+    describe: "Add new note",
+    handler: function () {
+        console.log("Adding a new note.")
+    }
+})
 
-if (command === "add") {
-    console.log("Add note")
-} else if (command === "remove"){
-    console.log("Remove note")
-} else {
-    console.error("Wrong command")
-}
+yargs.command({
+    command: "remove",
+    describe: "Remove the note",
+    handler: function () {
+        console.log("Removing the note.")
+    }
+})
 
+yargs.command({
+    command: "list",
+    describe: "Listing the notes",
+    handler: function () {
+        console.log("Listing out all your notes.")
+    }
+})
+
+yargs.command({
+    command: "read",
+    describe: "Read the note",
+    handler: function () {
+        console.log("Reading a note.")
+    }
+})
+
+getNote()
---
+++ notes.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+
+const getNote = function () {
+    console.log("Showing the note")
+}
+
+module.exports = {
+    getNote: getNote
+}
