93e6ef1080df703f084a0bd8d31b85b05b5cc44c
Denys Pugachov <pugachovdenys@gmail.com>
2022-12-24 14:57:45 +0100

    #20 add remove note func

--- app.js
+++ app.js
@@ -1,59 +1,67 @@
 // const chalk = require("chalk")
 // const yargs = require("yargs")
+const { alias } = require('yargs')
 const yargs = require('yargs')
 const notes = require("./notes.js")
 
 
 
 yargs
-.command({
-    command: "add",
-    describe: "Add new note",
-    builder: {
-        title: {
-            describe: "Note title",
-            alias: "-t", 
-            demandOption: true,
-            type: "string"
+    .command({
+        command: "add",
+        describe: "Add new note",
+        builder: {
+            title: {
+                describe: "Note title",
+                alias: "-t",
+                demandOption: true,
+                type: "string"
+            },
+            body: {
+                describe: "Note body",
+                alias: "-b",
+                demandOption: true,
+                type: "string",
+            }
         },
-        body: {
-            describe: "Note body",
-            alias: "-b",
-            demandOption: true,
-            type: "string",
+        handler: function (argv) {
+            notes.addNote(argv.title, argv.body)
         }
-    },
-    handler: function (argv) {
-        notes.addNote(argv.title, argv.body)
-    }
-})
+    })
 
 yargs
-.command({
-    command: "remove",
-    describe: "Remove the note",
-    handler: function () {
-        console.log("Removing the note.")
-    }
-})
+    .command({
+        command: "remove",
+        describe: "Remove the note",
+        bundler: {
+            title: {
+                describe: "Title of the note to delete.",
+                demandOption: true,
+                type: "string",
+            }
+        },
+        handler: function (argv) {
+            notes.removeNote(argv.title)
+        }
+    })
 
 yargs
-.command({
-    command: "list",
-    describe: "Listing the notes",
-    handler: function () {
-        console.log("Listing out all your notes.")
-    }
-})
+    .command({
+        command: "list",
+        describe: "Listing the notes",
+        handler: function () {
+            console.log("Listing out all your notes.")
+        }
+    })
 
 yargs
-.command({
-    command: "read",
-    describe: "Read the note",
-    handler: function () {
-        console.log("Reading a note.")
-    }
-})
+    .command({
+        command: "read",
+        describe: "Read the note",
+        handler: function () {
+            console.log("Reading a note.")
+        }
+    })
 
 //need call to get yarn.command work
 yargs.parse()
--- notes.js
+++ notes.js
@@ -1,27 +1,6 @@
 const { notStrictEqual } = require("assert")
 const fs = require("fs")
 
-const getNote = function () {
-    console.log("Showing the note")
-}
-
-const addNote = function (title, body) {
-    const notesArr = loadNotes()
-    //check for duplicate titles in arr
-    const duplicateNotesArr = notesArr.filter(n => {
-        return n.title === title
-    })
-
-    if (duplicateNotesArr.length === 0) {
-        //no duplicates
-        notesArr.push({ title: title, body: body })
-        saveNotes(notesArr)
-        console.log(`New note with title: "${title}" was successfully added!`)
-    } else {
-        console.log(`"${title}" is already taken. Please choose another title.`)
-    }
-}
-
 
 const loadNotes = function () {
     try {
@@ -34,15 +13,46 @@ const loadNotes = function () {
     }
 }
 
+
 const saveNotes = function (notesArr) {
     const notesStr = JSON.stringify(notesArr)
     fs.writeFileSync("notesDB.json", notesStr)
-    // console.log('Form save notest notesArr :>> ', notesArr);
+}
+
+
+const getNote = function () {
+    console.log("Showing the note")
+}
+
+
+const addNote = function (title, body) {
+    const notesArr = loadNotes()
+    //check for duplicate titles in arr
+    const duplicateNotesArr = notesArr.filter(n => {
+        return n.title === title
+    })
+    if (duplicateNotesArr.length === 0) {
+        //no duplicates
+        notesArr.push({ title: title, body: body })
+        saveNotes(notesArr)
+        console.log(`New note with title: "${title}" was successfully added!`)
+    } else {
+        console.log(`"${title}" is already taken. Please choose another title.`)
+    }
+}
+
+const removeNote = function (title) {
+    const notesArr = loadNotes()
+    const newNotesArr = notesArr.filter(n => {
+        return n.title != title
+    })
+    saveNotes(newNotesArr)
 }
 
 
 module.exports = {
     getNote: getNote,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote,
 }
 
